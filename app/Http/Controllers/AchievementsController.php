947e41f25f376c1ca234f060f729d073d99424ec
tharindarodrigo <tharindarodrigo@gmail.com>
2016-05-17 09:51:31 +0530

    Added CRUD to some components

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AchievementsController extends Controller
 {
-    public function createInterest(Request $request)
+    public function createAchievement(Request $request)
     {
         $achievement = new Achievement();
         $achievement->achievement = $request->get('achievement');
@@ -21,13 +21,13 @@ class AchievementsController extends Controller
         }
     }
 
-    public function getInterests()
+    public function getAchievements()
     {
         $achievement = Achievement::where('users_id', Auth::id())->get();
         return response($achievement);
     }
 
-    public function deleteInterest(Request $request)
+    public function deleteAchievement(Request $request)
     {
         if (Achievement::where('id', $request->get('id'))->where('users_id', Auth::id())->delete()) {
             return $this->getInterests();
--- app/Http/Controllers/EducationalGoalsController.php
+++ app/Http/Controllers/EducationalGoalsController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\EducationalGoal;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 
 class EducationalGoalsController extends Controller
 {
-    //
+    public function createEducationalGoal(Request $request)
+    {
+        $educational_goal = new EducationalGoal();
+        $educational_goal->educational_goal = $request->get('educational_goal');
+        $educational_goal->users_id = Auth::user()->id;
+
+        if ($educational_goal->save()) {
+            return $this->getEducationalGoals();
+        }
+    }
+
+    public function getEducationalGoals()
+    {
+        $educational_goal = EducationalGoal::where('users_id', Auth::id())->get();
+        return response($educational_goal);
+    }
+
+    public function deleteEducationalGoal(Request $request)
+    {
+        if (EducationalGoal::where('id', $request->get('id'))->where('users_id', Auth::id())->delete()) {
+            return $this->getEducationalGoals();
+        }
+    }
 }
